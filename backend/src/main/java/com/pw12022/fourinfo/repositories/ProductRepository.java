bc534fdbe887816e390e1afcce76fb0e666a0b48
Paulo Pacheco <51030749+paulopachecocel@users.noreply.github.com>
2022-12-15 20:16:07 -0300

    Backend finalizado_Renato

--- backend/src/main/java/com/pw12022/fourinfo/repositories/ProductRepository.java
+++ backend/src/main/java/com/pw12022/fourinfo/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.pw12022.fourinfo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pw12022.fourinfo.entities.Product;
 
 public interface ProductRepository extends JpaRepository <Product, Long>{
-    
+    List<Product>findAllByOrderByNameAsc();
 }
